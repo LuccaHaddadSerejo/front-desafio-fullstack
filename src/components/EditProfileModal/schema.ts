ae5ec7e0cbe5c3ec8d0f3463b592720fd3079911
LuccaHaddadSerejo <hslucca@gmail.com>
2023-06-05 01:52:31 -0300

    hotfix: changing the schema for updating profile

--- src/components/EditProfileModal/schema.ts
+++ src/components/EditProfileModal/schema.ts
@@ -7,14 +7,7 @@ const schema = z.object({
     .nonempty('O telefone é obrigatório')
     .regex(/^\d+$/, 'O telefone deve conter apenas números'),
   email: z.string().nonempty().email('Email inválido'),
-  password: z
-    .string()
-    .nonempty('A senha é obrigatória')
-    .min(8, 'A senha deve ter no mínimo 8 caracteres')
-    .regex(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
-      'A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial'
-    ),
+  password: z.string().optional(),
 });
 
 type UpdateData = z.infer<typeof schema>;
